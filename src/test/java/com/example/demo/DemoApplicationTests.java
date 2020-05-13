0bc608b5ec310f39f897e73ecb0da30d6f768baf
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-05-13 21:12:43 +0700

    initialize project

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
