3bed3e73f25051eb04881711dd80d18e35a12e19
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-05-14 20:36:51 +0700

    add dummy entity

--- src/main/java/com/example/demo/domain/GPS.java
+++ src/main/java/com/example/demo/domain/GPS.java
@@ -1,8 +1,49 @@
 package com.example.demo.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-// @Entity
+@Entity
 public class GPS {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "metadata")
+    private String metadata;
+
+    @Column(name = "waypoint")
+    private String waypoint;
+
+    @Column(name = "track")
+    private String track;
+
+    public String getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(String metadata) {
+        this.metadata = metadata;
+    }
+
+    public String getWaypoint() {
+        return waypoint;
+    }
+
+    public void setWaypoint(String waypoint) {
+        this.waypoint = waypoint;
+    }
+
+    public String getTrack() {
+        return track;
+    }
+
+    public void setTrack(String track) {
+        this.track = track;
+    }
 
 }
